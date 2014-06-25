56ab39c6e5382b5dd659dad7a545250d07df0e25
Pedro Lucas <LUCAS@LUCAS-PC.(none)>
2014-06-25 04:09:42 -0500

    Probar Spring Security para el proyecto considerando datos quemados en el descriptor xml de los parametros de seguridad e intersecciones de prueba con las paginas que ya estan creadas.

--- Project/sysmedic/src/main/java/com/swii/sysmedic/Views/UsersView.java
+++ Project/sysmedic/src/main/java/com/swii/sysmedic/Views/UsersView.java
@@ -8,7 +8,6 @@ package com.swii.sysmedic.Views;
 
 import com.swii.sysmedic.Facades.UsersFacade;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -16,11 +15,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import org.jasig.cas.client.authentication.*;
-import org.jasig.cas.client.session.SingleSignOutHandler;
 /**
  *
  * @author LUCAS
