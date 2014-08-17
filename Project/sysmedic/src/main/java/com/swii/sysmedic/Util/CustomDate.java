b01a2ba0d94a4950714067b1f33d8550ffc87514
Pedro Lucas <pitter_sdb@hotmail.com>
2014-08-17 17:26:51 -0500

    En la pestania de Turnos ahora se muestra la fecha del dia de hoy de manera automatica.

---
+++ Project/sysmedic/src/main/java/com/swii/sysmedic/Util/CustomDate.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.swii.sysmedic.Util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author LUCAS
+ */
+public class CustomDate {
+
+    private Date date;
+    private String[] monthNames = {"Enero", "Febrero", "Marzo", "Abrill", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Deciembre"};
+    
+    /**
+     * Creates a new instance of CustomDate
+     */
+    public CustomDate() {
+        date = Calendar.getInstance().getTime();
+    }
+    
+    public int getDate(){        
+        return date.getDate();
+    }
+    
+    public int getYear(){        
+        return date.getYear() + 1900;
+    }
+    
+    public String getMonthName(){        
+        return monthNames[date.getMonth()];
+    }
+    
+}
